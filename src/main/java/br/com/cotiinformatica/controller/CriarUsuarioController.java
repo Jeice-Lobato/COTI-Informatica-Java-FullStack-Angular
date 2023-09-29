5e51d13fa7fa7e4e16291b93594f04de40d8b6a3
Jeice-Lobato <jeicelobato@gmail.com>
2023-09-29 18:05:48 -0300

    feat: Mapeamento das rotas de autenticação, criação de usuário e recuperação de senha.

---
+++ src/main/java/br/com/cotiinformatica/controller/CriarUsuarioController.java
@@ -0,0 +1,25 @@
+package br.com.cotiinformatica.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class CriarUsuarioController {
+
+
+	// mapeamento da rota da página de criação de usuário
+	@RequestMapping(value = "/criar-usuario")
+	public ModelAndView criarUsuario() {
+
+
+		// definindo a página que será aberta no navegador:
+		// WEB-INF/views/criar-usuario.jsp
+		ModelAndView modelAndView 
+= new ModelAndView("criar-usuario");
+		return modelAndView;
+	}
+}
+
